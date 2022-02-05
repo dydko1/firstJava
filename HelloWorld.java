6b69a2f48c68b87e02aaee6851a6c26293382748
dydko1 <52148916+dydko1@users.noreply.github.com>
2022-02-05 07:58:43 +0100

    Update HelloWorld.java
    branch 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,22 @@ public class HelloWorld {
   public static void main(String[] args) throws InterruptedException {
     Thread.sleep(1000);
     System.out.println("Hello, World branch 2");
-    System.out.println("Hello, Miro branch 2");
     Thread.sleep(1000);
+    System.out.println("Hello, World branch 2");
+    Thread.sleep(1000);
+    System.out.println("Hello, World branch 2");
+    Thread.sleep(1000);
+    System.out.println("Hello, World branch 2");
+    Thread.sleep(1000);
+    System.out.println("Hello, World branch 2");
+    Thread.sleep(1000);
+    System.out.println("Hello, World branch 2");
+    Thread.sleep(1000);
+    System.out.println("Hello, World branch 2");
+    Thread.sleep(1000);
+    System.out.println("Hello, World branch 2");
+    Thread.sleep(1000);
+    System.out.println("Hello, World branch 2");
+
   }
 }
