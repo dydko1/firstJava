c38033e897d75d41b3710dba8a3b8077c8851a07
dydko1 <52148916+dydko1@users.noreply.github.com>
2022-02-05 07:21:59 +0100

    Update HelloWorld.java
    Mirek comments added

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,9 @@
  */
 public class HelloWorld {
   public static void main(String[] args) {
+    Thread.sleep(1000);
     System.out.println("Hello, World");
+    System.out.println("Hello, Miro");
+    Thread.sleep(1000);
   }
 }
