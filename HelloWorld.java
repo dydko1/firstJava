3695350db4aaef9acedacb3c99d3ad5a80a15b97
dydko1 <52148916+dydko1@users.noreply.github.com>
2022-02-05 07:55:02 +0100

    Update HelloWorld.java
    latest update

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,8 +5,16 @@
 public class HelloWorld {
   public static void main(String[] args) {
     Thread.sleep(1000);
-    System.out.println("Hello, World");
-    System.out.println("Hello, Miro");
+    System.out.println("Hello, World No. 1");
     Thread.sleep(1000);
+    System.out.println("Hello, World No. 2");
+    Thread.sleep(1000);
+    System.out.println("Hello, World No. 3");
+    Thread.sleep(1000);
+    System.out.println("Hello, World No. 4");
+        Thread.sleep(1000);
+    System.out.println("Hello, World No. 5");
+        Thread.sleep(1000);
+    System.out.println("Hello, World No. 6");
   }
 }
