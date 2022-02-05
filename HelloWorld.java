d5c908dcb794fdc14c348962d3547919683d741a
mirek <miroslaw.dyduch@gmail.com>
2022-02-05 07:49:34 +0100

    classure added

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,8 @@
-using System.Threading;
 /** 
  * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
  */
 public class HelloWorld {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
     Thread.sleep(1000);
     System.out.println("Hello, World branch 2");
     System.out.println("Hello, Miro branch 2");
